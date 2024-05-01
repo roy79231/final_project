a5026aa2c1fcd873a6855f88af1adafa61d46c66
timlin940 <aa0909095679@gmail.com>
2024-05-01 21:28:17 +0800

    timlin-type2 ready to connect

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,6 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\achievement;
+use App\Models\game_ending;
+use App\Models\game_process;
+use App\Models\normal_event;
+use App\Models\special_event;
+use App\Models\talent;
+use App\Models\achievement_event;
+use App\Models\achievement_fins;
+use App\Models\dead_event;
 
 class GameController extends Controller
 {
@@ -19,9 +28,7 @@ class GameController extends Controller
     public function start(){
         return view('start');
     }
-    public function event(){
-        return view('monthlyevent');
-    }
+    
     public function finish(){
         return view('finish');
     }
--- resources/views/monthlyevent.blade.php
+++ resources/views/monthlyevent.blade.php
@@ -1,34 +1,47 @@
 @extends('layouts.app')
 @section('content')
 <style>
-    .container{
-        background-color: beige;
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f8f9fa;
+        color: #333;
+        margin: 0;
+        padding: 0;
+    }
+    .container {
+        max-width: 1400px;
+        margin: 20px auto;
+        padding: 20px;
+        background-color:beige;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     }
     .month_number{
         background-color:beige;
         font-size: 30px; 
     
     }
-    .main_area{
+    .main_area {
         display: flex;
-        background-color: beige;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 20px;
     }
     .month_attribute{
         font-size: 30px;
         font-family:;
         text-align: center;
-       
         width: 400px;
     }
     .month_content{
         text-align: center;
         font-size: 20px;
-        width:400px;
+        width:600px;
     }
     .attribute_change{
         font-size: 30px;
         font-family: 'Courier New', Courier, monospace;
-       
+        text-align: center;
         width: 150px;
     }
     .month_move_last{
@@ -37,23 +50,13 @@
         align-items: center;
         text-align: center;
         width: 100px;
-        background-repeat: no-repeat;
-        background-color: transparent;
-        
     }
     
     .month_move_next{
         display: flex;
-       
         align-items: center;
         text-align: center;
         width: 100px;
-        
-    }
-    .month_move_skip{
-       
-        text-align: center;
-        
     }
 </style>
 <script>
@@ -65,21 +68,18 @@
     var monthhandsome=[5,5,5,5,5,5];
     var monthluck=[6,4,1,9,7,0];
     var monthethical=[9,7,6,4,3,1];
-    var monthhappy=Math.floor(Math.random()*10);
-    var monthcontent=['你通過學測進入中央大學了，你非常興奮，你決定一定要充實大學生活。','吃我雞雞',"好吃雞雞","晚餐吃雞雞","吃你雞雞","亂吃雞雞"];
-
+    var monthhappy=[1,2,3,4,4,4,5];
+    var monthcontent=['你通過學測進入中央大學了，你非常興奮，你決定一定要充實大學生活。','可供食用的動物乳汁。《紅樓夢》第一四回：「及收拾完備，更衣盥手，吃了兩口奶子，糖粳米粥。」',"好吃雞雞","晚餐吃雞雞","吃你雞雞","亂吃雞雞"];
+    const colors = ['green','red'];
 </script>
+
 <div class="container">
-    
             <div class="month_number" id="months">
                 <p>第 1 個月</p>
             </div>
             <div class="main_area">
                 <div class="month_move_last">
-                    <div class="last">
-                        
-                        <button style="font-size:24px"><i class="fa fa-angle-left" id="month_last" style="font-size: 80px"></i></button>
-                    </div>
+                    <button style="font-size:24px" id="month_last">上一月</button>
                 </div>
                 <div class="month_attribute">
                     <p class="month_intelligence" id="intelligence">
@@ -98,7 +98,7 @@
                         道德: <script>document.write(monthethical[0])</script>
                     </p>
                     <p class="month_happy" id="happy">
-                        快樂: <script>document.write(monthhappy)</script>
+                        快樂: <script>document.write(monthhappy[0])</script>
                     </p>
                 </div>
                 <div class="attribute_change">
@@ -121,9 +121,7 @@
                     </p>
                 </div>
                 <div class="month_move_next">
-                    <div class="next">
-                        <button style="font-size:24px"><i class="fa fa-angle-right" id="month_next" style="font-size: 80px"></i></button>
-                    </div>
+                    <button style="font-size:24px" id="month_next">下一月</button>
                 </div>
             </div>
             
@@ -146,7 +144,7 @@
             document.getElementById("handsome").innerHTML = "顏值: "+monthhandsome[count];
             document.getElementById("luck").innerHTML = "幸運: "+monthluck[count];
             document.getElementById("ethical").innerHTML = "道德: "+monthethical[count];
-            document.getElementById("happy").innerHTML = "快樂: "+monthhappy;
+            document.getElementById("happy").innerHTML = "快樂: "+monthhappy[count];
             document.getElementById('content').innerHTML = monthcontent[count];
 
             document.getElementById("intelligencechange").innerHTML="";
@@ -154,6 +152,7 @@
             document.getElementById("handsomechange").innerHTML="";
             document.getElementById("luckchange").innerHTML="";
             document.getElementById("ethicalchange").innerHTML="";
+            document.getElementById("happychange").innerHTML="";
             endEvent();
         }
         else{
@@ -163,37 +162,55 @@
             document.getElementById("handsome").innerHTML = "顏值: "+monthhandsome[count];
             document.getElementById("luck").innerHTML = "幸運: "+monthluck[count];
             document.getElementById("ethical").innerHTML = "道德: "+monthethical[count];
-            document.getElementById("happy").innerHTML = "快樂: "+monthhappy;
+            document.getElementById("happy").innerHTML = "快樂: "+monthhappy[count];
             document.getElementById('content').innerHTML = monthcontent[count];
             if(monthintelligence[count]-monthintelligence[count-1]>=0){
-                document.getElementById("intelligencechange").innerHTML="  ( +"+(monthintelligence[count]-monthintelligence[count-1]+" )");
+                document.getElementById("intelligencechange").innerHTML="   +"+(monthintelligence[count]-monthintelligence[count-1]);
+                document.getElementById("intelligencechange").style.color='green';
             }
             else{
-                document.getElementById("intelligencechange").innerHTML="  ( "+(monthintelligence[count]-monthintelligence[count-1]+" )");
+                document.getElementById("intelligencechange").innerHTML="   "+(monthintelligence[count]-monthintelligence[count-1]);
+                document.getElementById("intelligencechange").style.color='red';
             }
             if(monthweathly[count]-monthweathly[count-1]>=0){
-                document.getElementById("wealthychange").innerHTML="  ( +"+(monthweathly[count]-monthweathly[count-1]+" )");
+                document.getElementById("wealthychange").innerHTML="   +"+(monthweathly[count]-monthweathly[count-1]);
+                document.getElementById("wealthychange").style.color = 'green';
             }
             else{
-                document.getElementById("wealthychange").innerHTML="  ( "+(monthweathly[count]-monthweathly[count-1]+" )");
+                document.getElementById("wealthychange").innerHTML="   "+(monthweathly[count]-monthweathly[count-1]);
+                document.getElementById("wealthychange").style.color = 'red';
             }
             if(monthhandsome[count]-monthhandsome[count-1]>=0){
-                document.getElementById("handsomechange").innerHTML="  ( +"+(monthhandsome[count]-monthhandsome[count-1]+" )");
+                document.getElementById("handsomechange").innerHTML="   +"+(monthhandsome[count]-monthhandsome[count-1]);
+                document.getElementById('handsomechange').style.color = 'green';
             }
             else{
-                document.getElementById("handsomechange").innerHTML="  ( "+(monthhandsome[count]-monthhandsome[count-1]+" )");
+                document.getElementById("handsomechange").innerHTML="   "+(monthhandsome[count]-monthhandsome[count-1]);
+                document.getElementById('handsomechange').style.color='red';
             }
             if(monthluck[count]-monthluck[count-1]>=0){
-                document.getElementById("luckchange").innerHTML="  ( +"+(monthluck[count]-monthluck[count-1]+" )");
+                document.getElementById("luckchange").innerHTML="   +"+(monthluck[count]-monthluck[count-1]);
+                document.getElementById('luckchange').style.color= 'green';
             }
             else{
-                document.getElementById("luckchange").innerHTML="  ( "+(monthluck[count]-monthluck[count-1]+" )");
+                document.getElementById("luckchange").innerHTML="   "+(monthluck[count]-monthluck[count-1]);
+                document.getElementById('luckchange').style.color = 'red';
             }
             if(monthethical[count]-monthethical[count-1]>=0){
-                document.getElementById("ethicalchange").innerHTML="  ( +"+(monthethical[count]-monthethical[count-1]+" )");
+                document.getElementById("ethicalchange").innerHTML="   +"+(monthethical[count]-monthethical[count-1]);
+                document.getElementById('ethicalchange').style.color = 'green';
             }
             else{
-                document.getElementById("ethicalchange").innerHTML="  ( "+(monthethical[count]-monthethical[count-1]+" )");
+                document.getElementById("ethicalchange").innerHTML="   "+(monthethical[count]-monthethical[count-1]);
+                document.getElementById('ethicalchange').style.color = 'red';
+            }
+            if(monthhappy[count]-monthhappy[count-1]>=0){
+                document.getElementById("happychange").innerHTML = "   +"+(monthhappy[count]-monthhappy[count-1]);
+                document.getElementById('happychange').style.color = 'green';
+            }
+            else{
+                document.getElementById('happychange').innerHTML = "   "+(monthhappy[count]-monthhappy[count-1]);
+                document.getElementById('happychange').style.color = 'red';
             }
         }
     }
@@ -204,6 +221,8 @@
         count-=1;
         if(monthnumber<=0){
         alert("時光不能繼續回朔了");
+        monthnumber=1;
+        count=0;
         }
         else{
             document.getElementById("months").innerHTML = "第 "+monthnumber+" 個月";
@@ -212,7 +231,71 @@
             document.getElementById("handsome").innerHTML = "顏值: "+monthhandsome[count];
             document.getElementById("luck").innerHTML = "幸運: "+monthluck[count];
             document.getElementById("ethical").innerHTML = "道德: "+monthethical[count];
-            document.getElementById("happy").innerHTML = "快樂: "+monthhappy;
+            document.getElementById("happy").innerHTML = "快樂: "+monthhappy[count];
+            document.getElementById('content').innerHTML = monthcontent[count];
+            if(monthnumber!=1){
+                if(monthintelligence[count]-monthintelligence[count-1]>=0){
+                document.getElementById("intelligencechange").innerHTML="   +"+(monthintelligence[count]-monthintelligence[count-1]);
+                document.getElementById("intelligencechange").style.color='green';
+                }
+                else{
+                    document.getElementById("intelligencechange").innerHTML="   "+(monthintelligence[count]-monthintelligence[count-1]);
+                    document.getElementById("intelligencechange").style.color='red';
+                }
+                if(monthweathly[count]-monthweathly[count-1]>=0){
+                    document.getElementById("wealthychange").innerHTML="   +"+(monthweathly[count]-monthweathly[count-1]);
+                    document.getElementById("wealthychange").style.color = 'green';
+                }
+                else{
+                    document.getElementById("wealthychange").innerHTML="   "+(monthweathly[count]-monthweathly[count-1]);
+                    document.getElementById("wealthychange").style.color = 'red';
+                }
+                if(monthhandsome[count]-monthhandsome[count-1]>=0){
+                    document.getElementById("handsomechange").innerHTML="   +"+(monthhandsome[count]-monthhandsome[count-1]);
+                    document.getElementById('handsomechange').style.color = 'green';
+                }
+                else{
+                    document.getElementById("handsomechange").innerHTML="   "+(monthhandsome[count]-monthhandsome[count-1]);
+                    document.getElementById('handsomechange').style.color='red';
+                }
+                if(monthluck[count]-monthluck[count-1]>=0){
+                    document.getElementById("luckchange").innerHTML="   +"+(monthluck[count]-monthluck[count-1]);
+                    document.getElementById('luckchange').style.color= 'green';
+                }
+                else{
+                    document.getElementById("luckchange").innerHTML="   "+(monthluck[count]-monthluck[count-1]);
+                    document.getElementById('luckchange').style.color = 'red';
+                }
+                if(monthethical[count]-monthethical[count-1]>=0){
+                    document.getElementById("ethicalchange").innerHTML="   +"+(monthethical[count]-monthethical[count-1]);
+                    document.getElementById('ethicalchange').style.color = 'green';
+                }
+                else{
+                    document.getElementById("ethicalchange").innerHTML="   "+(monthethical[count]-monthethical[count-1]);
+                    document.getElementById('ethicalchange').style.color = 'red';
+                }
+                if(monthhappy[count]-monthhappy[count-1]>=0){
+                    document.getElementById("happychange").innerHTML = "   +"+(monthhappy[count]-monthhappy[count-1]);
+                    document.getElementById('happychange').style.color = 'green';
+                }
+                else{
+                    document.getElementById('happychange').innerHTML = "   "+(monthhappy[count]-monthhappy[count-1]);
+                    document.getElementById('happychange').style.color = 'red';
+                }
+            }
+            else{
+                document.getElementById("intelligencechange").innerHTML="";
+                
+                document.getElementById("wealthychange").innerHTML="";
+            
+                document.getElementById("handsomechange").innerHTML="";
+            
+                document.getElementById("luckchange").innerHTML="";
+    
+                document.getElementById("ethicalchange").innerHTML="";
+
+                document.getElementById("happychange").innerHTML="";
+            }
         }
     }
 
