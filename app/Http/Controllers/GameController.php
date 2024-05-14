a1a26e1f750d8d075f3331e56c1a6d2a8296cb91
roy79231 <roy82859370@gmail.com>
2024-05-15 02:31:54 +0800

    fix_achievement_fin_in_gamecontroller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -523,12 +523,16 @@ class GameController extends Controller
         //這個foreach有問題要修 已解決
         if(!empty($accomplish_achievements)){
             for($i=0;$i<count($accomplish_achievements);$i++){
-
+                // 检查是否存在相同的记录
+                $existing_record = achievement_fins::where('user_id', $user_id)
+                ->where('achievement_id', $accomplish_achievements[$i])
+                ->first();
+                if(!$existing_record){
                     achievement_fins::create([
                         'user_id'=> $user_id,
                         'achievement_id'=> $accomplish_achievements[$i],
                     ]);
-
+                }
             };
         }
         $game_processes = game_process::where('user_id',$user_id)->get();
