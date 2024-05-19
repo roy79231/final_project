b41cf46c42bc4c7b6c358a314886653425d401e8
Eddie114114 <eddie105117@gmail.com>
2024-05-20 02:35:59 +0800

    finishBladeOnePlusTwo

--- app/Http/Controllers/TalentController.php
+++ app/Http/Controllers/TalentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\talent;
 use Illuminate\Support\Facades\DB;
-use App\Models\talent;
 
 class TalentController extends Controller
 {
