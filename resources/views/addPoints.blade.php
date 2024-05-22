c95fcaf15b8e4971a2d534775e39e9d4df789058
aaronName <wuxuanwei01@gmail.com>
2024-05-22 12:04:53 +0800

    revise addPoint style 2

--- resources/views/addPoints.blade.php
+++ resources/views/addPoints.blade.php
@@ -10,37 +10,8 @@
         list-style-type: none;
     }
 
-    .nav-pills .nav-link {
-        border-radius: 2px;
-    }
-
-    .justify-content-end .nav-link {
-        color: black !important;
-    }
-
-    .navbar-light .navbar-nav .nav-link {
-        color: rgba(0, 0, 0);
-    }
-
-    .centered-container, .mt-5 {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-
-    .mt-5 h2, .text-center{
-        text-align: center;
-    }
-
-    a {
-        color: blue;
-        text-decoration: none;
-    }
-
-    .separator {
-        border-left: 1px solid #ccc;
-        height: 100%;
+    .remaining-points {
+        border-right: #000 solid 1px;
     }
 
     @media (max-width: 767.98px) {
@@ -49,8 +20,25 @@
             border-bottom: 1px solid #000;
         }
     }
-    .container li {
-        font-size: 1.5rem;
+
+    .addPoints-container {
+        background-color: rgb(237, 218, 191);
+        min-height: 80vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .addPoints-container h2 {
+        font-size: 3rem;
+    }
+
+    .addPoints-container li {
+        font-size: 2.5rem;
+        margin: 20px;
+    }
+
+    .navbar {
+        z-index: 100;
     }
 
     .talent-choice {
@@ -58,12 +46,15 @@
         display: flex;
         flex-direction: column;
         justify-content: space-between;
+        z-index: 1;
     }
 
     .action-buttons {
         display: flex;
         flex-direction: column;
         align-items: flex-end;
+        margin-top: auto;
+        margin-right: 70px;
     }
 
     .reset-btn, .action-buttons .bet-btn, .action-buttons .start-btn{
@@ -71,7 +62,7 @@
         background-color: transparent;
         margin-left: 5px;
         margin-bottom: 5px;
-        font-size: 1.8rem;
+        font-size: 2rem;
     }
 
     .bet-btn {
@@ -90,19 +81,20 @@
         display: inline-block;
         width: 2em;
         text-align: center;
+        font-size: 2.5rem;
     }
 
-    .form-check-input {
-        width: 1.5rem !important;
-        height: 1.5rem !important;
+    .talent-form-check-input {
+        width: 2.5rem !important;
+        height: 2.5rem !important;
     }
-    .form-check-label {
-        font-size: 1.5rem;
+    .talent-form-check-label {
+        font-size: 2.5rem;
     }
 </style>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 @section('content')
-<div class="container min-vh-100 d-flex flex-column">
+<div class="addPoints-container min-vh-100 d-flex flex-column">
     <div class="row d-flex align-items-stretch">
         <div class="col-12 col-md-6 remaining-points">
             <!-- Left Content -->
@@ -140,38 +132,61 @@
                 <button class="reset-btn btn-lg" id="reset-btn">歸零</button>
             </div>
         </div>
-        <div class="col talent-choice d-flex flex-column">
-            <div class="col-12 col-md-6">
-                <!-- Right Content -->
-                <div class="p-3">
-                    <h2>選擇天賦: </h2>
-                    <form id="talent-form">
-                        <div id="talent-options">
-                            @foreach ($talents as $talent)
-                                @if(in_array($talent->id,$rand_nums))
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="talent" value="{{$talent->id}}">
-                                        <label class="form-check-label" >{{ $talent->name }}</label>
-                                    </div>
-                                @endif
-                            @endforeach
-                        </div>
-                    </form>
-                </div>
-                <div class="action-buttons">
-                    <button class="bet-btn">我是賭狗</button> <br>
-                    <!-- style="display: none"-->
-                    <form action="{{ route('run') }}" method="POST">
-                        @csrf
-                        <input type="number" id='intelligence2' name="intelligence" placeholder="intelligence" required style="display: none"><br>
-                        <input type="number" id='wealth2' name="wealth" placeholder="wealth" required style="display: none"><br>
-                        <input type="number" id='appearance2' name="appearance" placeholder="appearance" required style="display: none"><br>
-                        <input type="number" id='luck2' name="luck" placeholder="luck" required style="display: none"><br>
-                        <input type="number" id='morality2' name="morality" placeholder="morality" required style="display: none"><br>
-                        <input type="hidden" id='talent2' name="talent" placeholder="name" required style="display: none"></textarea> <br>
-                        <button  class="start-btn" type="submit">開始大學</button>
-                    </form>
-                </div>
+        <?php
+        $rand_nums = [];
+            for($i=0;$i<4;$i++){
+                while(true){
+                    $num = rand(1,$talents_len);
+                    if(!in_array($num,$rand_nums)){
+                        array_push($rand_nums,$num);
+                        break;
+                    }
+                }
+            }
+            if(isset($_POST['change_btn'])){
+                echo '1';
+                $rand_nums = [];
+                print('嗨');
+                for($i=0;$i<4;$i++){
+                    while(true){
+                        $num = rand(1,$talents_len) ;
+                        if(!in_array($num,$rand_nums)){
+                            array_push($rand_nums,$num);
+                            break;
+                        }
+                    }
+                }
+            }
+        ?>
+        <div class="col-12 col-md-6 d-flex flex-column">
+            <div class="p-3">
+                <h2>選擇天賦: </h2>
+                <form id="talent-form">
+                    <div id="talent-options">
+                        @foreach ($talents as $talent)
+                            @if(in_array($talent->id,$rand_nums))
+                                <div class="talent-form-check">
+                                    <label class="talent-form-check-label" >
+                                        <input class="talent-form-check-input" type="radio" name="talent" value="{{$talent->id}}">{{ $talent->name }}
+                                    </label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </form>
+            </div>
+            <div class="action-buttons mt-auto align-self-end">
+                <button class="bet-btn">我是賭狗</button> <br>
+                <form action="{{ route('run') }}" method="POST">
+                    @csrf
+                    <input type="number" id='intelligence2' name="intelligence" placeholder="intelligence" required style="display: none"><br>
+                    <input type="number" id='wealth2' name="wealth" placeholder="wealth" required style="display: none"><br>
+                    <input type="number" id='appearance2' name="appearance" placeholder="appearance" required style="display: none"><br>
+                    <input type="number" id='luck2' name="luck" placeholder="luck" required style="display: none"><br>
+                    <input type="number" id='morality2' name="morality" placeholder="morality" required style="display: none"><br>
+                    <input type="hidden" id='talent2' name="talent" placeholder="name" required style="display: none"></textarea> <br>
+                    <button  class="start-btn" type="submit">開始大學</button>
+                </form>
             </div>
         </div>
 
