d89147dec0be8e95e8d05f9e96f0e2b5fad6849b
Eddie114114 <eddie105117@gmail.com>
2024-05-23 22:11:16 +0800

    finish33333333

--- resources/views/finish3.blade.php
+++ resources/views/finish3.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+    <?php
+        $finishIntelligence = ["中央大學店", "普普通通", "資優生", "炸魚電神"];
+        $finishAppearance = ["中央哥布林", "Banana", "Cherry", "Date"];
+        $finishMorality = ["Apple", "Banana", "Cherry", "Date"];
+        $finishLuck = ["Apple", "Banana", "Cherry", "Date"];
+        $finishWealth = ["Apple", "Banana", "Cherry", "Date"];
+        $finishHappiness = ["Apple", "Banana", "Cherry", "Date"];
+        $finishScore = ["Apple", 
+                        "Banana", 
+                        "Cherry", 
+                        "你是智慧的結晶，你是文明的瑰寶，你是人類的獨苗，你是上帝的遺珠，你是最後的希望，你是電你是光你是唯一的神話"
+                        ];
+    ?>
+
     <div class="finishContainerThr">
         <div class="finishBoxThr1">
             <div class="finishInnerBox1">
@@ -9,7 +23,7 @@
                         @if($graduate)
                             畢業 
                         @else
-                            沒畢業
+                            你從中央高歌離席了
                         @endif
                     </div>
                 </div>
@@ -17,12 +31,21 @@
             <div class="finishInnerBox2">
                 <div class="finishCenterBox">
                     <div class="finishTxtScore">
-                        Your Score: 69
+                        <?php $total = $end->intelligence + $end->appearance + $end->morality + $end->luck + $end->wealth + $end->happiness; ?>
+                        Your Score: {{ $total }}
                     </div>
                 </div>
                 <div class="finishCenterBox">
                     <div class="finishTxtScore2">
-                        你是智慧的結晶，你是文明的瑰寶，你是人類的獨苗，你是上帝的遺珠，你是最後的希望，你是電你是光你是唯一的神話
+                        @if ($total <= 5)
+                            {{ finishScore[0] }}
+                        @elseif ($total <= 10)
+                            {{ finishScore[1] }}
+                        @elseif ($total <= 15)
+                            {{ finishScore[2] }}
+                        @else
+                            {{ finishScore[3] }}
+                        @endif
                     </div>
                 </div>
             </div>
@@ -38,42 +61,90 @@
         <div class="finishBoxThr2">
             <div class="finishInnerBox4">
                 <div class="finishTxtRight">
-                    智商: 
+                    智商: {{ $end->intelligence }}
                 </div>
                 <div class="finishTxtRight">
-                    你是傻逼，嗚嗚嗚
+                    @if ($end->intelligence <= 5)
+                        {{ $finishIntelligence[0] }}
+                    @elseif ($end->intelligence <= 10)
+                        {{ $finishIntelligence[1] }}
+                    @elseif ($end->intelligence <= 15)
+                        {{ $finishIntelligence[2] }}
+                    @else
+                        {{ $finishIntelligence[3] }}
+                    @endif
                 </div>
                 <div class="finishTxtRight">
-                    道德: 
+                    道德: {{ $end->morality }}
                 </div>
                 <div class="finishTxtRight">
-                    你是傻逼，嗚嗚嗚
+                    @if ($end->morality <= 5)
+                        {{ $finishMorality[0] }}
+                    @elseif ($end->morality <= 10)
+                        {{ $finishMorality[1] }}
+                    @elseif ($end->morality <= 15)
+                        {{ $finishMorality[2] }}
+                    @else
+                        {{ $finishMorality[3] }}
+                    @endif
                 </div>
                 <div class="finishTxtRight">
-                    幸運: 
+                    幸運: {{ $end->luck }}
                 </div>
                 <div class="finishTxtRight">
-                    你是傻逼，嗚嗚嗚
+                    @if ($end->luck <= 5)
+                        {{ $finishLuck[0] }}
+                    @elseif ($end->luck <= 10)
+                        {{ $finishLuck[1] }}
+                    @elseif ($end->luck <= 15)
+                        {{ $finishLuck[2] }}
+                    @else
+                        {{ $finishLuck[3] }}
+                    @endif
                 </div>
             </div>
             <div class="finishInnerBox5">
                 <div class="finishTxtRight">
-                    財富: 
+                    財富: {{ $end->wealth }}
                 </div>
                 <div class="finishTxtRight">
-                    你是傻逼，嗚嗚嗚
+                    @if ($end->wealth <= 5)
+                        {{ $finishWealth[0] }}
+                    @elseif ($end->wealth <= 10)
+                        {{ $finishWealth[1] }}
+                    @elseif ($end->wealth <= 15)
+                        {{ $finishWealth[2] }}
+                    @else
+                        {{ $finishWealth[3] }}
+                    @endif
                 </div>
                 <div class="finishTxtRight">
-                    顏值: 
+                    顏值: {{ $end->appearance }}
                 </div>
                 <div class="finishTxtRight">
-                    你是傻逼，嗚嗚嗚
+                    @if ($end->appearance <= 5)
+                        {{ $finishAppearance[0] }}
+                    @elseif ($end->intelligence <= 10)
+                        {{ $finishAppearance[1] }}
+                    @elseif ($end->appearance <= 15)
+                        {{ $finishAppearance[2] }}
+                    @else
+                        {{ $finishAppearance[3] }}
+                    @endif
                 </div>
                 <div class="finishTxtRight">
-                    快樂: 
+                    快樂: {{ $end->happiness }}
                 </div>
                 <div class="finishTxtRight">
-                    你是傻逼，嗚嗚嗚
+                    @if ($end->happiness <= 5)
+                        {{ $finishHappiness[0] }}
+                    @elseif ($end->happiness <= 10)
+                        {{ $finishHappiness[1] }}
+                    @elseif ($end->happiness <= 15)
+                        {{ $finishHappiness[2] }}
+                    @else
+                        {{ $finishHappiness[3] }}
+                    @endif
                 </div>
             </div>
         </div>
