81b496c86070e7747114b4352ee640a2ae3b15c1
Eddie114114 <eddie105117@gmail.com>
2024-05-27 21:18:44 +0800

    finishkjdnfkjsdnf

--- resources/views/finish3.blade.php
+++ resources/views/finish3.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <?php
         $finishIntelligence = ["中央大學店", "普普通通", "資優生", "炸魚電神"];
-        $finishAppearance = ["中央哥布林", "Banana", "Cherry", "Date"];
-        $finishMorality = ["Apple", "Banana", "Cherry", "Date"];
-        $finishLuck = ["Apple", "Banana", "Cherry", "Date"];
-        $finishWealth = ["Apple", "Banana", "Cherry", "Date"];
-        $finishHappiness = ["Apple", "Banana", "Cherry", "Date"];
-        $finishScore = ["Apple", 
-                        "Banana", 
-                        "Cherry", 
+        $finishAppearance = ["中央哥布林", "普普通通", "早餐店阿姨覺得你很帥", "皎如玉樹臨風起"];
+        $finishMorality = ["我爸是警察", "普普通通", "喜提一張好人卡", "你站在道德制高點"];
+        $finishLuck = ["你的臉超級黑", "普普通通", "心想事成", "神眷者"];
+        $finishWealth = ["公園是我家", "普普通通", "家境小康", "隱形亞洲富豪"];
+        $finishHappiness = ["憂鬱青年", "普普通通", "你很快樂", "浪味仙~快樂似神仙~"];
+        $finishScore = ["你揮一揮衣袖，輕輕的走了，沒有在中央大學留下任何痕跡也沒有帶走什麼", 
+                        "你在這段時光留下了一抹屬於你的色彩，或許對其他人來說無足輕重，但這會成為你心中的一筆濃墨", 
+                        "你在中央劃下了重重的一刀，", 
                         "你是智慧的結晶，你是文明的瑰寶，你是人類的獨苗，你是上帝的遺珠，你是最後的希望，你是電你是光你是唯一的神話"
                         ];
     ?>
@@ -37,14 +37,14 @@
                 </div>
                 <div class="finishCenterBox">
                     <div class="finishTxtScore2">
-                        @if ($total <= 5)
-                            {{ finishScore[0] }}
-                        @elseif ($total <= 10)
-                            {{ finishScore[1] }}
-                        @elseif ($total <= 15)
-                            {{ finishScore[2] }}
+                        @if ($total <= 25)
+                            {{ $finishScore[0] }}
+                        @elseif ($total <= 35)
+                            {{ $finishScore[1] }}
+                        @elseif ($total <= 45)
+                            {{ $finishScore[2] }}
                         @else
-                            {{ finishScore[3] }}
+                            {{ $finishScore[3] }}
                         @endif
                     </div>
                 </div>
@@ -64,11 +64,11 @@
                     智商: {{ $end->intelligence }}
                 </div>
                 <div class="finishTxtRight">
-                    @if ($end->intelligence <= 5)
+                    @if ($end->intelligence <= 8)
                         {{ $finishIntelligence[0] }}
-                    @elseif ($end->intelligence <= 10)
+                    @elseif ($end->intelligence <= 16)
                         {{ $finishIntelligence[1] }}
-                    @elseif ($end->intelligence <= 15)
+                    @elseif ($end->intelligence <= 24)
                         {{ $finishIntelligence[2] }}
                     @else
                         {{ $finishIntelligence[3] }}
@@ -78,11 +78,11 @@
                     道德: {{ $end->morality }}
                 </div>
                 <div class="finishTxtRight">
-                    @if ($end->morality <= 5)
+                    @if ($end->morality <= 8)
                         {{ $finishMorality[0] }}
-                    @elseif ($end->morality <= 10)
+                    @elseif ($end->morality <= 16)
                         {{ $finishMorality[1] }}
-                    @elseif ($end->morality <= 15)
+                    @elseif ($end->morality <= 24)
                         {{ $finishMorality[2] }}
                     @else
                         {{ $finishMorality[3] }}
@@ -92,11 +92,11 @@
                     幸運: {{ $end->luck }}
                 </div>
                 <div class="finishTxtRight">
-                    @if ($end->luck <= 5)
+                    @if ($end->luck <= 8)
                         {{ $finishLuck[0] }}
-                    @elseif ($end->luck <= 10)
+                    @elseif ($end->luck <= 16)
                         {{ $finishLuck[1] }}
-                    @elseif ($end->luck <= 15)
+                    @elseif ($end->luck <= 24)
                         {{ $finishLuck[2] }}
                     @else
                         {{ $finishLuck[3] }}
@@ -108,11 +108,11 @@
                     財富: {{ $end->wealth }}
                 </div>
                 <div class="finishTxtRight">
-                    @if ($end->wealth <= 5)
+                    @if ($end->wealth <= 8)
                         {{ $finishWealth[0] }}
-                    @elseif ($end->wealth <= 10)
+                    @elseif ($end->wealth <= 16)
                         {{ $finishWealth[1] }}
-                    @elseif ($end->wealth <= 15)
+                    @elseif ($end->wealth <= 24)
                         {{ $finishWealth[2] }}
                     @else
                         {{ $finishWealth[3] }}
@@ -122,11 +122,11 @@
                     顏值: {{ $end->appearance }}
                 </div>
                 <div class="finishTxtRight">
-                    @if ($end->appearance <= 5)
+                    @if ($end->appearance <= 8)
                         {{ $finishAppearance[0] }}
-                    @elseif ($end->intelligence <= 10)
+                    @elseif ($end->intelligence <= 16)
                         {{ $finishAppearance[1] }}
-                    @elseif ($end->appearance <= 15)
+                    @elseif ($end->appearance <= 24)
                         {{ $finishAppearance[2] }}
                     @else
                         {{ $finishAppearance[3] }}
@@ -136,11 +136,11 @@
                     快樂: {{ $end->happiness }}
                 </div>
                 <div class="finishTxtRight">
-                    @if ($end->happiness <= 5)
+                    @if ($end->happiness <= 6)
                         {{ $finishHappiness[0] }}
-                    @elseif ($end->happiness <= 10)
+                    @elseif ($end->happiness <= 16)
                         {{ $finishHappiness[1] }}
-                    @elseif ($end->happiness <= 15)
+                    @elseif ($end->happiness <= 24)
                         {{ $finishHappiness[2] }}
                     @else
                         {{ $finishHappiness[3] }}
